89b77487c67bb1cd859856132933510cde0609b5
shein <msh86.02.18@rambler.ru>
2021-03-09 16:06:36 +0500

    init

---
+++ src/main/java/shein/dmitriy/post/post/controllers/PostController.java
@@ -0,0 +1,20 @@
+package shein.dmitriy.post.post.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class PostController {
+
+    @Autowired
+    public PostController() {
+    }
+
+    @GetMapping
+    public String fvfPage() {
+        return "post_main";
+    }
+}
