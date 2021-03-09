a69be7c7bac6ba9b5473a796c2637e8f6b6ec920
shein <msh86.02.18@rambler.ru>
2021-03-09 16:37:26 +0500

    создал liter

---
+++ src/main/java/shein/dmitriy/post/post/entytis/Liter.java
@@ -0,0 +1,29 @@
+package shein.dmitriy.post.post.entytis;
+
+import lombok.Data;
+
+@Data
+public class Liter {
+
+    private String shpi;
+    private String index;
+    private String country;
+    private String addresstype;
+    private String numaddresstype;
+    private String region;
+    private String area;
+    private String city;
+    private String street;
+    private String house;
+    private String litter;
+    private String corpus;
+    private String room;
+    private String adresat;
+    private String mass;
+    private String price;
+    private String payment;
+    private String comment;
+    private String teladdress;
+    private String numContract;
+    private String smsnoticre;
+}
