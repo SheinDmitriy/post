b8bf485d19ae14f7218ec126d6d5a24bef405809
shein <msh86.02.18@rambler.ru>
2021-03-24 09:53:28 +0500

    добавил вывод добавленных писем добавил кнопку удаления письма добавил кнопку очистки

--- src/main/java/shein/dmitriy/post/post/controllers/PostController.java
+++ src/main/java/shein/dmitriy/post/post/controllers/PostController.java
@@ -5,17 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import shein.dmitriy.post.post.entytis.Liter;
 import shein.dmitriy.post.post.services.LiterService;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/")
 public class PostController {
-    private List literList;
     private LiterService literService;
 
     @Autowired
@@ -26,13 +24,28 @@ public class PostController {
     @GetMapping
     public String fvfPage(Model model) {
         model.addAttribute("liter", new Liter());
+        model.addAttribute("liters", literService.getLiterList());
         return "post_main";
     }
 
-    @PostMapping("/form")
+    @PostMapping("/add")
     public String newLiter(Liter liter, BindingResult result, Model model) {
-        literService.add(liter, literList);
-        model.addAttribute("liter", new Liter());
+        if (result.hasErrors()) {
+            return "redirect:/";
+        }
+        literService.add(liter);
+        return "redirect:/";
+    }
+
+    @PostMapping("/remove/{shpi}")
+    public String remove(@PathVariable(value = "shpi") String shpi, Model model) {
+        literService.remove(shpi);
+        return "redirect:/";
+    }
+
+    @PostMapping("/clear")
+    public String clear(Model model) {
+        literService.clear();
         return "redirect:/";
     }
 }
--- src/main/java/shein/dmitriy/post/post/services/LiterService.java
+++ src/main/java/shein/dmitriy/post/post/services/LiterService.java
@@ -1,20 +1,46 @@
 package shein.dmitriy.post.post.services;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import shein.dmitriy.post.post.entytis.Liter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Data
 public class LiterService {
-
+    private static List<Liter> literList = new ArrayList<>();
 
     @Autowired
     public LiterService() {
     }
 
-    public void add(Liter liter, List literList) {
+    public static void add(Liter liter) {
         literList.add(liter);
+    }
+
+    public static List<Liter> getLiterList() {
+        return literList;
+    }
+
+    public void remove(String shpi) {
+        Liter tmp = new Liter();
+        for (Liter l: literList) {
+            if (l.getShpi().equals(shpi)){
+                tmp = l;
+                break;
+            } else {
+                tmp = null;
+            }
+        }
+        if (tmp != null){
+            literList.remove(tmp);
+        }
+    }
+
+    public void clear() {
+        literList.clear();
     }
 }
